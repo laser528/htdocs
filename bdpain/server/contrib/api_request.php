19da4cf95ab648b49f9d4f3bd43bd9a2d28a7d80
nreid1994 <nicholasreid@google.com>
2023-07-11 02:46:56 -0400

    Initial contrib files.
    Setup API Request
    Initial ENV Variable and constants
    Rate Limit API Request

--- bdpain/server/contrib/api_request.php
+++ bdpain/server/contrib/api_request.php
@@ -1,3 +1,37 @@
 <?php
 /* Make calls to BDPA API. */
+require_once "./constants.php";
+require_once "./rate_limit.php";
+
+function fetch($endpoint, $method ="GET", $payload = null) {
+    $opt = array(
+        CURLOPT_URL => "{$_ENV['API_URL']}/{$_ENV['API_VERSION']}/{$endpoint}",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTPHEADER => [
+            "Content-type: application/json",
+            "Accept: application/json",
+            "Access-Control-Allow-Origin: *",
+            "Access-Control-Allow-Credentials: true",
+            "Authorization: bearer {$_ENV['API_KEY']}",
+        ],
+        CURLOPT_CUSTOMREQUEST => $method,
+        CURLOPT_SSL_VERIFYHOST => 0,
+        CURLOPT_SSL_VERIFYPEER => 0,
+    );
+
+    if (payload) $opt[CURLOPT_POSTFIELDS] = json_encode($payload);
+
+    echo print_r($opt);
+
+    $ch = curl_init();
+    curl_setopt_array($ch, $opt);
+
+    $limiter = RateLimiter::getInstance();
+    $limiter->await();
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    return $response;
+}
+
 ?>
--- bdpain/server/contrib/lib.php
+++ bdpain/server/contrib/lib.php
@@ -1,3 +1,21 @@
 <?php
 /* Generic PHP Functions. */
+require_once "./constants.php";
+
+/**
+ * @param string $password users password
+ * @param hex_string $salt users salt. Must be a Hexadecimal string.
+ */
+function generate_salt_and_key($password, $user_salt = null) {
+    $salt  = hex2bin($salt) ?: openssl_random_pseudo_bytes(SALT_BYTES);
+    $key = hash_pbkdf2(KEY_ALGORITHM, $password, $salt, KEY_GEN_ITERATIONS, KEY_LENGTH_BYTES);
+
+    return (object) array("salt" => bin2hex($salt), "key" => bin2hex($key));
+}
+
+/** Used at the top of files to set php headers. */
+function set_headers() {
+    header('Content-type: application/json');
+    header("Access-Control-Allow-Origin: *");
+}
 ?>
