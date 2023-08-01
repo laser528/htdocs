9f235622975f43fdafa4fccaff8ac851d48cb2a3
nreid1994 <nicholasreid@google.com>
2023-08-01 02:20:04 -0400

    lol

--- bdpain/server/contrib/test.php
+++ bdpain/server/contrib/test.php
@@ -1,6 +1,12 @@
 <?php
 require_once "../contrib/lib.php";
 
-echo force_logout("64c7087e9b804e5c6e0af658");
+$data = (object) array("user_id" => "64c7087e9b804e5c6e0af658");
+
+if (isset($data->user_id) && force_logout($data->user_id)) {
+    echo "Hi";
+    force_logout_response();
+    exit();
+}
 
 ?>
