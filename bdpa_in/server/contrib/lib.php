19e788bcbe098f561f42b15ee5aebfa4093b78dd
nreid1994 <nicholasreid@google.com>
2023-08-12 17:35:45 -0400

    Added ability to call BDPA API.

--- bdpa_in/server/contrib/lib.php
+++ bdpa_in/server/contrib/lib.php
@@ -1,10 +1,42 @@
 <?php
 /* Generic PHP Functions. */
 require_once "constants.php";
+require_once "rate_limit.php";
 
 /** Gets a new Mysqli Instance. */
 function get_mysql_connection() {
     return new mysqli($_ENV["DB_HOST"], $_ENV["DB_USER"], $_ENV["DB_PASSWORD"], $_ENV["DB_NAME"]);
 }
 
+/** Make a call to BDPA API. */
+function bdpa_fetch($endpoint, $method = "GET", $payload = null) {
+    $opt = array(
+        CURLOPT_URL => "{$_ENV['API_URL']}/{$_ENV['API_VERSION']}/{$endpoint}",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTPHEADER => [
+            "Content-type: application/json",
+            "Accept: application/json",
+            "Access-Control-Allow-Origin: *",
+            "Access-Control-Allow-Credentials: true",
+            "Authorization: bearer {$_ENV['API_KEY']}",
+        ],
+        CURLOPT_CUSTOMREQUEST => $method,
+        CURLOPT_SSL_VERIFYHOST => 0,
+        CURLOPT_SSL_VERIFYPEER => 0,
+    );
+
+    echo $opt[CURLOPT_URL];
+
+    if ($payload) $opt[CURLOPT_POSTFIELDS] = json_encode($payload);
+
+    $ch = curl_init();
+    curl_setopt_array($ch, $opt);
+
+    $limiter = RateLimiter::getInstance();
+    $limiter->await();
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    return $response;
+}
 ?>
