9af32c7a5f2a25104c38d67914e1b3454ba81019
nreid1994 <nicholasreid@google.com>
2023-08-12 17:20:56 -0400

    Added .env file with app constants. Added ability to ready file on server

---
+++ bdpa_in/server/contrib/constants.php
@@ -0,0 +1,17 @@
+<?php
+/** Holds constants used server wide. */
+
+# Load env variables.
+$environment_path = realpath(__DIR__."/../../.env");
+$file_open = fopen($environment_path, 'r');
+
+if($file_open) {
+    while($line = fgets($file_open)) {
+        [$key, $value] = explode("=", $line);
+        $_ENV[$key] = trim($value);
+    }
+    fclose($file_open);
+}
+
+echo print_r($_ENV);
+?>
