0dd281f4f719c25d4863f7a00e107b36b0cb799f
nreid1994 <nicholasreid@google.com>
2023-08-13 13:48:20 -0400

    Final DB Schema for App

--- bdpa_in/database/bdpa_in.sql
+++ bdpa_in/database/bdpa_in.sql
@@ -1,57 +1,68 @@
 # Tables for application.
 
-create table users (user_id varchar(200) not null PRIMARY key,
-                    username varchar(200) not null unique,
-                    email varchar(200) not null unique,
-                    password varchar(200) not null,
-                    salt varchar(200) not null,
-                    views int(10) not null,
-                    type ENUM('inner', 'staff', 'administrator') not null,
-                    url varchar(200) not null unique,
-                    cover_photo_location varchar(200) null,
-                    createdAt int(10) not null,
-                    updatedAt int(10) not null
-                   );
-                   
-create table profile_about (
-    user_id varchar(200) not null primary key,
-    about text not null,
-    FOREIGN KEY(user_id) references users(user_id) on delete cascade on update cascade
-    );
-    
-create table profile_skills (
-    user_id varchar(200) not null,
-    skill varchar(50) not null,
+CREATE TABLE `users` (
+  `user_id` varchar(200) NOT NULL PRIMARY KEY,
+  `username` varchar(200) NOT NULL UNIQUE,
+  `email` varchar(200) NOT NULL UNIQUE,
+  `salt` varchar(200) NOT NULL,
+  `views` varchar(200) NOT NULL,
+  `type` enum('inner','staff','administrator') NOT NULL,
+  `url` varchar(200) NOT NULL UNIQUE,
+  `cover_photo_location` varchar(200) DEFAULT NULL,
+  `createdAt` varchar(200) NOT NULL,
+  `updatedAt` varchar(200) NOT NULL
+);
+
+CREATE TABLE `connections` (
+  `user_id` varchar(200) NOT NULL,
+  `connection_id` varchar(200) NOT NULL,
+  PRIMARY KEY(user_id, connection_id),
+  FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY(connection_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE `opportunities` (
+  `opportunity_id` varchar(200) NOT NULL PRIMARY KEY,
+  `creator_id` varchar(200) NOT NULL,
+  `title` mediumtext NOT NULL,
+  `contents` mediumtext NOT NULL,
+  `views` varchar(200) NOT NULL,
+  `createdAt` varchar(200) NOT NULL,
+  `updatedAt` varchar(200) NOT NULL
+);
+
+CREATE TABLE `profile_about` (
+  `user_id` varchar(200) NOT NULL PRIMARY KEY,
+  `about` mediumtext NOT NULL,
+   FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE `profile_skills` (
+  `user_id` varchar(200) NOT NULL,
+  `skill` varchar(50) NOT NULL,
     PRIMARY KEY(user_id, skill),
-    FOREIGN KEY(user_id) references users(user_id) on delete cascade on update cascade
+    FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE `profile_user_section` (
+  `user_id` varchar(200) NOT NULL,
+  `type` enum('experience','education','volunteering') NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `location` mediumtext NOT NULL,
+  `description` mediumtext NOT NULL,
+  `startedAt` varchar(200) NOT NULL,
+  `endedAt` varchar(200) DEFAULT NULL,
+    PRIMARY KEY (user_id, type, title),
+    FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE `force_logout` (
+    user_id varchar(200) not null primary key,
+    FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE
     );
     
-create table profile_user_section (
-    user_id varchar(200) not null,
-    type ENUM('experience','education','volunteering'),
-    title varchar(200) not null,
-    location text not null,
-    description text not null,
-    startedAt int(10) not null,
-    endedAt int(10),
-    PRIMARY KEY(user_id, type, title),
-    FOREIGN KEY(user_id) references users(user_id) on delete cascade on update cascade
-    );
-    
-create table opportunities(
-    opportunity_id varchar(200) not null primary key,
-    creator_id varchar(200) not null,
-    title text not null,
-    contents text not null,
-    views int(10) not null,
-    createdAt int(10) not null,
-    updatedAt int(10) not null
-    );
-    
- create table connections(
-     user_id varchar(200) not null,
-     connection_id varchar(200) not null,
-     PRIMARY KEY(user_id, connection_id),
-     FOREIGN KEY(user_id) REFERENCES users(user_id) on delete cascade on update cascade,
-     FOREIGN KEY(connection_id) REFERENCES users(user_id) on delete cascade on update cascade
-     );
+CREATE TABLE `forgot_password` (
+  user_id varchar(200) not null primary key,
+  token varchar(200) not null unique,
+  FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
