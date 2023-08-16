290d17522dd3273fa95e59f30b54b79d97d7b3de
nreid1994 <nicholasreid@google.com>
2023-08-16 14:44:38 -0400

    FrontEnd
    Logic for App User Service.

---
+++ bdpa_in/src/contrib/lib.ts
@@ -0,0 +1,19 @@
+export enum UserType {
+  GUEST = "guest",
+  INNER = "inner",
+  STAFF = "staff",
+  ADMINISTRATOR = "administrator",
+}
+
+export interface User {
+  user_id: string;
+  username: string;
+  email: string;
+  salt: string;
+  views: string;
+  type: UserType;
+  cover_photo_location?: string;
+  createdAt: string;
+  updatedAt: string;
+  url: string;
+}
